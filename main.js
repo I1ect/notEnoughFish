eec39524f9f9946b3523f1aabb7ed4625f02a324
picept9@gmail.com <picept9@gmail.com>
2023-11-26 22:50:41 -0500

    fishing rod tiers, fixed shrimp sell

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const variables = {
     fishingSpeedLevel: 1,
     fishingVolumeLevel: 1,
-    fishingQualityLevel: 1,
+    fishingRod: "Starter Rod",
     sellMult: 1,
     level: 1,
     xp: 0,
@@ -20,6 +20,11 @@ const sellConversions = {
 const xpConversions = {
     Minnows: 1,
     Shrimp: 3
+}
+
+const starterRodMultipliers = {
+    Minnows: 0.9,
+    Shrimp: 0.1
 }
 
 function Update() {
@@ -46,13 +51,20 @@ function Update() {
 
 // Functioning Buttons
 function Fish() {
-    variables.Minnows += 1
+    var randomVal = Math.random();
+    if (randomVal < starterRodMultipliers.Minnows) {
+        variables.Minnows += 1
+    }
+    else {
+        variables.Shrimp += 1
+    }
 }
 
 function Sell() {
     variables.cash += variables.sellMult * (variables.Minnows * sellConversions.Minnows)
     variables.xp += (variables.Minnows * xpConversions.Minnows)
     variables.Minnows = 0
+    variables.Shrimp = 0
 }
 
 setInterval(Update, 100)
